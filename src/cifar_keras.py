452640158ecea517dfc6fa00de24d18b26feda6c
dondakeshimo <went.went.takkun135@gmail.com>
2018-08-01 22:28:54 +0900

    [fix]Dropoutをふんだんに散りばめ、pooling層を減らす

--- src/cifar_keras.py
+++ src/cifar_keras.py
@@ -36,23 +36,25 @@ y_test = to_categorical(y_test, num_classes=CLASSES)
 
 model = Sequential()
 
-model.add(Conv2D(filters=32, kernel_size=5, strides=2,
+model.add(Conv2D(filters=96, kernel_size=3, padding="same",
                  activation="relu", input_shape=X_train.shape[1:]))
 model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(filters=128, kernel_size=3, padding="same",
+model.add(Conv2D(filters=96, kernel_size=3, padding="same",
                  activation="relu"))
 model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(filters=512, kernel_size=3, padding="same",
+model.add(Conv2D(filters=256, kernel_size=3, padding="same",
                  activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=3, padding="same",
+model.add(Conv2D(filters=256, kernel_size=3, padding="same",
                  activation="relu"))
 model.add(Conv2D(filters=256, kernel_size=3, padding="same",
                  activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
 
 model.add(Flatten())
 model.add(Dense(128))
